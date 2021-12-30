ebe3923bc6330ad57529c1833d2d75ed1b7b4f2c
Nikita Kedrov <kedrov@Nikitas-MBP-2.lan>
2021-12-30 15:08:33 -0500

    Added new changes

--- features/step-definitions/then.js
+++ features/step-definitions/then.js
@@ -2,6 +2,21 @@ import { Then } from '@wdio/cucumber-framework';
 
 
 // TODO:
-Then(/^I expect something$/, async (page) => {
+Then(/^I expect to see the dashboard page$/, async (page) => {
+
+});
+
+// TODO:
+Then(/^I expect to see the allocations page$/, async (page) => {
+
+});
+
+// TODO:
+Then(/^I expect to see the employees page$/, async (page) => {
+
+});
+
+// TODO:
+Then(/^I expect to see the cards page$/, async (page) => {
 
 });
