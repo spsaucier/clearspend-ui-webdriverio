17df5d75c45dad023b1bfc4c0a66e6cdc22e3f70
mukhamad.davlatov <mukhamad.davlatov@clearspend.com>
2022-03-03 08:41:33 -0600

    locators for signUp fist page

--- pageobjects/sign.up.page.js
+++ pageobjects/sign.up.page.js
@@ -8,6 +8,13 @@ class HomePageTest extends Page {
      * define selectors using getter methods
      */
 
+    get inputFirstName() {return $('//input[@name="first-name"]'); }
+    get inputLastName() {return $('//input[@name="last-name"]'); }
+    get inputEmail() {return $('//input[@name="email"]'); }
+    get checkboxAge18() {return $('//label/input[@name="agree"]'); }
+    get buttonNext() {return $('//button[@class="LDgtd lhpHE onkol K4lLS"]'); }
+    
+
 }
 
 export default new ManageBalancePage();
