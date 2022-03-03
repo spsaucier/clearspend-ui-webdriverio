7512eb4e4264531dd7b882b871a6065e8c3c53e9
mukhamad.davlatov <mukhamad.davlatov@clearspend.com>
2022-03-03 10:14:15 -0600

    business Details POM locators

--- pageobjects/Onboarding/buseness.details.page.js
+++ pageobjects/Onboarding/buseness.details.page.js
@@ -3,7 +3,26 @@ import Page from './page';
 
 class BusinessDetails extends Page {
 
-    get headerBusninessDetails() { return $('//h1/span[contains(text(), "Business details")]"]'); }
+    get headerBusninessDetails() { return $('//h3[contains(text(),"Business details")]'); }
+    get inputLegalEntityName() {return $('//div/input[@name="business-name"]'); }
+    get inputBusinessEIN() {return $('//div/input[@name="business-ein"]'); }
+    get inputCorpPhoneNumber() {return $('//div/input[@name="corporate-phone-number"]'); }
+    get inputDescribeYourBusiness() {return $('//div/input[@name="business-description"]'); }
+    get dropdownMerchantCategory() {return $('//div/input[@name="business-mcc"]'); }
+    /**
+     * get list of merchant categories
+     */
+    get inputStreetAddress() {return $('//div/input[@name="streetLine1"]'); }
+    get inputAptUnit() {return $('//div/input[@name="streetLine2"]'); }
+    get inputCity() {return $('//div/input[@name="locality"]'); }
+    get dropdownState() {return $('//div/input[@name="state"]'); }
+    /**
+     * get list of states
+     */
+    get inputZipCode() {return $('//div/input[@name="postalCode"]'); }
+    get buttonNext() {return $('//button[@class="LDgtd K4lLS ZXfpb"]'); }
+    
+
 
    
     open() {
