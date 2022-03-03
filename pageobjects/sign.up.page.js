8bec4acf25edbf756a1d36dbb754d2afbbe94dae
mukhamad.davlatov <mukhamad.davlatov@clearspend.com>
2022-03-03 09:27:16 -0600

    added more elements to sign up

--- pageobjects/sign.up.page.js
+++ pageobjects/sign.up.page.js
@@ -13,7 +13,27 @@ class HomePageTest extends Page {
     get inputEmail() {return $('//input[@name="email"]'); }
     get checkboxAge18() {return $('//label/input[@name="agree"]'); }
     get buttonNext() {return $('//button[@class="LDgtd lhpHE onkol K4lLS"]'); }
-    
+
+    // sign up  " Next, we need to know about your business " page 
+
+    get radioIndividual() {return $('//label/span[contains(text(),"Individual/Sole Proprietorship")]'); }
+    get radioCompany() {return $('//label/span[contains(text(),"Company")]'); }
+
+    // business structure of Company legal entity
+
+    get dropdownOption() {return $('//div/input[@name="business-structure"]'); }
+    get radioNonProfitOrg() {return $('//label/span[contains(text(),"Nonprofit organization")]'); }
+
+    // signUp > "Where should we send a text to verify your mobile phone number? " page
+
+    get inputPhoneNumber() {return $('//div/input[@name="phone"]'); }
+
+    // signUp > "Let's create a super secure, super secret password" page
+
+    get inputpassword() {return $('//div/input[@name="new-password"]'); }
+    get inputConfirmPassword() {return $('//div/input[@name="confirm-password"]'); }
+    get buttonCreateAccount() {return $('//span[contains(text(),"Create Account")]'); }
+
 
 }
 
