30a27a9602bc3285023372f658b7e3137da518fc
mukhamad.davlatov <mukhamad.davlatov@clearspend.com>
2022-03-11 10:27:43 -0600

    Account Settings page scenarios with POM

--- pageobjects/accountSettings.page.js
+++ pageobjects/accountSettings.page.js
@@ -3,6 +3,12 @@ import Page from './page';
 class AccountSettingsPage extends Page {
 
     get headerAccountSettings() { return $('//div/span[@class="MzPU6"]'); }
+
+    get buttonUpdateProfile() {return $('//span[contains(text(),"Update Profile")]'); }
+    get buttonUpdatePassword() { return $('//span[contains(text(),"Update Password")]'); }
+    get buttonUpdatePhoneNumber() { return $('//span[contains(text(),"Update Phone Number")]'); }
+    get buttonSignOut() { return $('//span[contains(text(),"Sign Out")]'); }
+    
    
     open() {
         return super.open('profile');
