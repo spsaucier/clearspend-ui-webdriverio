0db9433e7d1d62d384d11152c342411f445d6120
Nikita Bogdanov <nikita.bogdanov@clearspend.com>
2022-04-28 15:06:12 -0400

    Changed to a different service to use for headless mode.

--- config/wdio.smoke.js
+++ config/wdio.smoke.js
@@ -58,8 +58,10 @@ exports.config = {
         //
         browserName: 'chrome',
         'goog:chromeOptions': {
-             headless: true
-         },
+            // to run chrome headless the following flags are required
+            // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+            args: ['--headless', '--disable-gpu'],
+            },
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
