1233d33d68188de40581623334e31f7ec86f6cb4
Nikita Bogdanov <nikita.bogdanov@clearspend.com>
2022-04-28 21:04:51 -0400

    Changed to use all the scripts to use

--- config/wdio.smoke.js
+++ config/wdio.smoke.js
@@ -21,7 +21,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        './features/**/cards.feature',
+        './features/smoke/**/*.feature',
     ],
     // Patterns to exclude.
     exclude: [
