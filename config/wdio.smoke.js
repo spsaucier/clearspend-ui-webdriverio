d7a82b0fda13a89479d368447c6ed776db20e249
Nikita Bogdanov <nikita.bogdanov@clearspend.com>
2022-04-28 21:02:31 -0400

    Switched to headless mode.

--- config/wdio.smoke.js
+++ config/wdio.smoke.js
@@ -57,9 +57,9 @@ exports.config = {
         maxInstances: 10,
         //
         browserName: 'chrome',
-         'goog:chromeOptions': {
-             headless: true
-         },
+        'goog:chromeOptions': {
+            args: ['--headless', '--disable-gpu'],
+            },
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
